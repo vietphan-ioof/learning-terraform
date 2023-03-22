d81e7d697ad6921bab971db26ef14eb3c4b5b289
Viet Phan <109930734+vietphan-ioof@users.noreply.github.com>
2023-03-22 16:01:21 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,38 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
+locals {
+  region  = "ap-southeast-2"
+  numList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  result  = sum([for x in local.numList : 10 * x if x % 2 == 0])
 
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+  resource_tags = {
+    project     = "PaaS",
+    environment = "prod"
   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+  required_tags = {
+    project     = var.project_name,
+    environment = var.environment
   }
 
-  owners = ["979382823631"] # Bitnami
+  tags = merge(local.required_tags, local.resource_tags)
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+# create random string
+resource "random_string" "main" {
+  length           = 20
+  lower            = true
+  upper            = true
+  numeric          = true
+  special          = true
+  override_special = "/@£$"
+}
 
-  tags = {
-    Name = "HelloWorld"
-  }
+# add random pet name
+resource "random_pet" "main" {
+  length = 4
+}
+
+# create local file with random contents
+resource "local_file" "main" {
+  filename = "${path.module}/foo.txt"
+  content  = random_string.main.result
 }
